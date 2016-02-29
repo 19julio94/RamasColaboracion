8fab38e37e276b0c1007ef2f9ebe33351beec841
agomezcastro <agomezcastro@atanasoff04.danielcastelao.org>
2016-02-29 09:20:13 +0100

    Multiplicacion y division de numeros

--- src/ramascolaboracion/Ejercicio.java
+++ src/ramascolaboracion/Ejercicio.java
@@ -1,5 +1,7 @@
 package ramascolaboracion;
 
+import javax.swing.JOptionPane;
+
 
 
 /**
@@ -7,5 +9,22 @@ package ramascolaboracion;
  * @author agomezcastro
  */
 public class Ejercicio {
+    private int num1, num2, res;
     
+
+    public int multiplicacion(){
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el 1er numero"));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el 2º numero"));
+        res =  num1 * num2;
+        System.out.println("Resultado= "+ res);
+        return res;
     }
+    
+    public int division(){    
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el 1er numero"));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el 2º numero"));
+        res =  num1 / num2;
+        System.out.println("Resultado= "+ res);
+        return res;
+    }
+}
